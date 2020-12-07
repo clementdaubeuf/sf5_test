7c932531decfc04c7f8a04061847aef885facb08
Clement Daubeuf <daubeufclement@gmail.com>
2020-12-07 01:24:14 +0100

    add migrations description

--- migrations/Version20201207002057.php
+++ migrations/Version20201207002057.php
@@ -14,7 +14,7 @@ final class Version20201207002057 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Add article';
     }
 
     public function up(Schema $schema) : void
